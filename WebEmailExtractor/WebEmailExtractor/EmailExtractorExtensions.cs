eb0ac2bc4dcd56c20c0b63b2c30356e712076954
srddrag <Srdjan.Dragic@extendhealth.com>
2016-11-10 15:40:16 -0700

    Adds GUI Windows Form

--- WebEmailExtractor/WebEmailExtractor/EmailExtractorExtensions.cs
+++ WebEmailExtractor/WebEmailExtractor/EmailExtractorExtensions.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace WebEmailExtractor
 {
@@ -10,22 +13,31 @@ namespace WebEmailExtractor
     {
         public static List<string> GetEmails(string url)
         {
-            var webCLient = new WebClient();
-            var data = webCLient.DownloadData(url);
-            var download = Encoding.ASCII.GetString(data);
+            try
+            {
+                var webCLient = new WebClient();
+                var data = webCLient.DownloadData(url);
+                var download = Encoding.ASCII.GetString(data);
 
-            download = download.Replace("<em>", "");
-            download = download.Replace("</em>", "");
-            download = download.Replace("<b>", "");
-            download = download.Replace("</b>", "");
+                download = download.Replace("<em>", "");
+                download = download.Replace("</em>", "");
+                download = download.Replace("<b>", "");
+                download = download.Replace("</b>", "");
 
-            System.Diagnostics.Debug.WriteLine(download);
+                Debug.WriteLine(download);
 
-            var collection = Regex.Matches(download, EmailRegex());
-            var distinct = collection.OfType<Match>().Select(m => m.Value).Distinct();
-            var foundEmails = distinct.Select(x => x.ToString()).ToList();
+                var collection = Regex.Matches(download, EmailRegex());
+                var distinct = collection.OfType<Match>().Select(m => m.Value).Distinct();
+                var foundEmails = distinct.Select(x => x.ToString()).ToList();
 
-            return foundEmails;
+                return foundEmails;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("The URL seems to be broken! Please try again or use another URL.");
+                if (Form.ActiveForm != null) Form.ActiveForm.Close();
+            }
+            return null;
         }
 
         private static string EmailRegex()
