02f78f7583f31a560616190515930eb2f0a68f07
srddrag <Srdjan.Dragic@extendhealth.com>
2016-11-10 09:23:17 -0700

    Refactors to clean up code, Handles if directory does not exist

--- WebEmailExtractor/WebEmailExtractor/EmailExtractor.cs
+++ WebEmailExtractor/WebEmailExtractor/EmailExtractor.cs
@@ -1,40 +1,68 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace WebEmailExtractor
 {
     public class EmailExtractor
     {
-        public static List<string> GetEmails(string url)
+        public static void ExtractEmails(string url)
         {
-            var webCLient = new WebClient();
-            var data = webCLient.DownloadData(url);
-            var download = Encoding.ASCII.GetString(data);
-
-            download = download.Replace("<em>", "");
-            download = download.Replace("</em>", "");
-            download = download.Replace("<b>", "");
-            download = download.Replace("</b>", "");
-
-            System.Diagnostics.Debug.WriteLine(download);
-
-            var collection = Regex.Matches(download, EmailRegex());
-
-            var distinct = collection.OfType<Match>().Select(m => m.Value).Distinct();
-            var foundEmails = distinct.Select(x => x.ToString()).ToList();
-
-            return foundEmails;
+            if (GetEmails(url).Count > 0)
+            {
+                WriteSuccessMessage();
+                WriteEmailsToFile(GetEmails(url));
+            }
+            else
+                WriteRejectionMessage();
         }
 
-        private static string EmailRegex()
+        private static void WriteSuccessMessage()
         {
-            return @"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
-                   + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
-                   + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
-                   + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})";
+            Console.WriteLine("OOOHHH Baby! We got something!!!");
+            Console.WriteLine();
+        }
+
+        private static List<string> GetEmails(string url)
+        {
+            return EmailExtractorExtensions.GetEmails(url);
+        }
+
+        private static void WriteEmailsToFile(IEnumerable<string> emails)
+        {
+            HandleDirectory(@"C:\emails");
+
+            using (var file = new StreamWriter($@"C:\emails\emails{DateTime.Now:yyyy-MM-dd HH.mm.ss}.txt"))
+            {
+                foreach (var email in emails)
+                {
+                    Console.WriteLine(email);
+                    file.Write(email);
+                    file.Write("\r\n");
+                }
+                WriteFooterWithExit();
+            }
+        }
+
+        private static void HandleDirectory(string directory)
+        {
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+        }
+
+        private static void WriteFooterWithExit()
+        {
+            Console.WriteLine();
+            Console.WriteLine("BAH HA HA HA, BAH HA HA HA, BAH HA HA HA, BAH HA HA HA");
+            Console.WriteLine("Give it to me!");
+
+            Console.ReadLine();
+        }
+
+        private static void WriteRejectionMessage()
+        {
+            Console.WriteLine("Sorry, Couldn't find them =[. Try another link please!");
+            Console.ReadLine();
         }
     }
 }
--- WebEmailExtractor/WebEmailExtractor/Program.cs
+++ WebEmailExtractor/WebEmailExtractor/Program.cs
@@ -10,53 +10,9 @@ namespace WebEmailExtractor
         {
             // Copy the website link and paste it in between quotes!
             // If the program crashes, just hold Ctrl + z and the code will go back to a working state.
-            var _url = "http://lgsdlgapk6.ss7.sharpschool.com/our_school/staff_directory";
+            const string url = "http://lgsdlgapk6.ss7.sharpschool.com/our_school/staff_directory";
 
-            ExtractEmails(_url);
-        }
-
-        private static void ExtractEmails(string url)
-        {
-            if (GetEmails(url).Count > 0)
-            {
-                Console.WriteLine("OOOHHH Baby! We got something!!!");
-                Console.WriteLine();
-
-                WriteEmailsToFile(GetEmails(url));
-            }
-            else
-            {
-                Console.WriteLine("Sorry, Couldn't find them =[. Try another link please!");
-                Console.ReadLine();
-            }
-        }
-
-        private static List<string> GetEmails(string url)
-        {
-            return EmailExtractor.GetEmails(url);
-        }
-
-        private static void WriteEmailsToFile(IEnumerable<string> emails)
-        {
-            using (var file = new StreamWriter($@"C:\emails\emails{DateTime.Now:yyyy-MM-dd HH.mm.ss}.txt"))
-            {
-                foreach (var email in emails)
-                {
-                    Console.WriteLine(email);
-                    file.Write(email);
-                    file.Write("\r\n");
-                }
-                WriteFooterWithExit();
-            }
-        }
-
-        private static void WriteFooterWithExit()
-        {
-            Console.WriteLine();
-            Console.WriteLine("BAH HA HA HA, BAH HA HA HA, BAH HA HA HA, BAH HA HA HA");
-            Console.WriteLine("Give it to me!");
-
-            Console.ReadLine();
+            EmailExtractor.ExtractEmails(url);
         }
     }
 }
