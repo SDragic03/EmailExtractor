7ad105a6d61994eb58353a9383cc4b386b5a1920
srddrag <Srdjan.Dragic@extendhealth.com>
2016-11-10 15:48:52 -0700

    Okay... Really adds GUI window form

---
+++ WebEmailExtractor/EmailExtractorApp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace EmailExtractorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnGetEmails = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(49, 138);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(333, 436);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnGetEmails
+            // 
+            this.btnGetEmails.Location = new System.Drawing.Point(416, 138);
+            this.btnGetEmails.Name = "btnGetEmails";
+            this.btnGetEmails.Size = new System.Drawing.Size(138, 64);
+            this.btnGetEmails.TabIndex = 1;
+            this.btnGetEmails.Text = "Get Emails!";
+            this.btnGetEmails.UseVisualStyleBackColor = true;
+            this.btnGetEmails.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(505, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Paste URL into textbox =]!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 606);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGetEmails);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Email Extractor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnGetEmails;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WebEmailExtractor/EmailExtractorApp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+using WebEmailExtractor;
+
+namespace EmailExtractorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            btnGetEmails.Enabled = false;
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                listBox1.Items.Add("Oops, looks like you forgot to paste in a URL =P!");
+                btnGetEmails.Enabled = true;
+
+            }
+            else if (EmailExtractorExtensions.GetEmails(textBox1.Text).Count > 0)
+            {
+                listBox1.Items.Add("OOOHHH Baby! We got something!!!");
+                listBox1.Items.Add("\r\n");
+
+                var emailCollection = EmailExtractorExtensions.GetEmails(textBox1.Text);
+
+                EmailToFileWriter.WriteEmailsToFile(emailCollection);
+                foreach (var email in emailCollection)
+                {
+                    listBox1.Items.Add(email);
+                }
+
+                listBox1.Items.Add("\r\n");
+                listBox1.Items.Add("Give it to me!");
+
+                btnGetEmails.Enabled = true;
+            }
+            else
+            {
+                listBox1.Items.Add("Sorry, Couldn't find them =[. Try another link please!");
+                btnGetEmails.Enabled = true;
+            }
+        }
+
+    }
+}
